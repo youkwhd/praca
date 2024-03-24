04bac91e778afe87f2bc22fee53566779d59c876
youkwhd <lolywk@tutanota.com>
2024-03-24 20:26:32 +0700

    feat: try getting jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { getRemoteJobs } from "@/lib/api/jobicy";
+import Image from "next/image";
+import { getRemoteJobs } from "@/api/jobicy";
 
 export default async () => {
+    const jobResult = await getRemoteJobs();
+
     return (
         <>
-            <h1>Praca</h1>
+            {jobResult.jobs.map((job: any) => {
+                return (
+                    <div key={job.id}>
+                        <p>{job.jobTitle}</p>
+                        <img src={job.companyLogo}
+                            alt="Company Logo"
+                            width={30}
+                            height={30} />
+                    </div>
+                );
+            })}
         </>
     );
 }
