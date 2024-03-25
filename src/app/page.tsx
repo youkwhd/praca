5e9150863c60fcd6018dff5ae23b99a976c99d22
youkwhd <lolywk@tutanota.com>
2024-03-25 12:53:15 +0700

    feat: use parseJob*, call the API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 import Image from "next/image";
-import { getRemoteJobs } from "@/api/jobicy";
+import { getRemoteJobs, parseJobType, parseJobGeo } from "@/api/jobicy";
 
 export default async () => {
-    const jobResult = await getRemoteJobs();
+    const jobResult = await getRemoteJobs("web-app-design");
 
     return (
-        <>
+        <main>
             {jobResult.jobs.map((job: any) => {
                 return (
-                    <div key={job.id}>
+                    <div key={job.id} className="my-5">
                         <p>{job.jobTitle}</p>
+                        <p>{job.companyName}</p>
                         <img src={job.companyLogo}
                             alt="Company Logo"
                             width={30}
                             height={30} />
+                        <p>{parseJobType(job.jobType[0])}</p>
+                        <ul>
+                            {parseJobGeo(job.jobGeo).map((geo: string) => {
+                                return (
+                                    <li>{geo}</li>
+                                );
+                            })}
+                        </ul>
+                        <p>{job.pubDate}</p>
                     </div>
                 );
             })}
-        </>
+        </main>
     );
 }
