5b01f689ee55fcb164962e9aa1772bbd25c365fb
youkwhd <lolywk@tutanota.com>
2024-03-25 18:41:03 +0700

    feat: render job grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,40 @@ export default async () => {
     const jobResult = await getRemoteJobs("web-app-design");
 
     return (
-        <main>
+        <main className="grid grid-cols-mmax-340 gap-4 p-5">
             {jobResult.jobs.map((job: any) => {
                 return (
-                    <div key={job.id} className="my-5">
-                        <p>{job.jobTitle}</p>
-                        <p>{job.companyName}</p>
-                        <img src={job.companyLogo}
-                            alt="Company Logo"
-                            width={30}
-                            height={30} />
-                        <p>{parseJobType(job.jobType[0])}</p>
-                        <ul>
-                            {parseJobGeo(job.jobGeo).map((geo: string) => {
-                                return (
-                                    <li>{geo}</li>
-                                );
-                            })}
-                        </ul>
-                        <p>{job.pubDate}</p>
+                    <div key={job.id} className="p-3 bg-praca-grey flex align-center rounded-md border border-praca-grey"
+                                      // style={{ width: 450 }}
+                                      >
+                        <div>
+                            <div className="m-1.5 flex align-center justify-center">
+                                <img className="rounded"
+                                    src={job.companyLogo}
+                                    alt="Company Logo"
+                                    width={40}
+                                    height={40} />
+                            </div>
+                        </div>
+                        <div className="mx-2">
+                            <div>
+                                <p className="font-semibold">{job.jobTitle}</p>
+                                <p className="text-sm">{job.companyName}</p>
+                            </div>
+                            <div>
+                                <p className="text-xs">{parseJobType(job.jobType[0])}</p>
+                                {/*
+                                <ul>
+                                    {parseJobGeo(job.jobGeo).map((geo: string) => {
+                                        return (
+                                            <li key={geo} className="text-xs">{geo}</li>
+                                        );
+                                    })}
+                                </ul>
+                                */}
+                                <p className="text-xs">{job.pubDate}</p>
+                            </div>
+                        </div>
                     </div>
                 );
             })}
